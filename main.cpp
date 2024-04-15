2d94ea7ae4124735213c90db9c1569d6ac8c6263
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-15 21:46:57 +0300

    first commit: add spedometer task solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int maint () {
+int main () {
+    // 16.6.1 speedometer
+    char speed_str[5];
+    double speed_value = 0, precision = 0.1, delta;
+    do {
+        std::cout << "Speed delta: ";
+        std::cin >> delta;
+        speed_value = (speed_value + delta <= 0 ? 0 : speed_value + delta);
+        speed_value = (speed_value <= 150 ? speed_value : 150);
+        std::sprintf(speed_str, "%.1f", speed_value);
+        std::cout << "Speed: " << speed_str << std::endl;
+    } while (speed_value >= precision || speed_value <= -precision);
+    std::cout << "You've stopped!" << std::endl;
 
 
+    return 0;
 }
